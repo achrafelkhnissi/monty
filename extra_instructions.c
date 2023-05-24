c1d2cba77e52ddabb51cdb6ddd61e3bfde214dde
Achraf El Khnissi <achraf.elkhnissi@gmail.com>
2023-05-24 12:57:16 +0100

    fix: extra function in file

---
+++ extra_instructions.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * _nop - doesn’t do anything
+ * @stack: pointer to the stack
+ * @line_number: line number
+ * Return: void
+ */
+void _nop(_stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
--- instructions.c
+++ instructions.c
@@ -154,15 +154,3 @@ void _add(_stack_t **stack, unsigned int line_number)
 	_pop(stack, line_number);
 	(*stack)->n = sum;
 }
-
-/**
- * _nop - doesn’t do anything
- * @stack: pointer to the stack
- * @line_number: line number
- * Return: void
- */
-void _nop(_stack_t **stack, unsigned int line_number)
-{
-	(void)stack;
-	(void)line_number;
-}
